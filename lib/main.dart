2c0a3a2d71e5c5ee6c2db25d132d49f1c78daede
parkgyeonghwan <ghksdlajwu@gmail.com>
2022-01-11 17:13:36 +0900

    AnimatedContainer연습

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'Screen/home_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyAnimatedContainer(),
     );
   }
 }
